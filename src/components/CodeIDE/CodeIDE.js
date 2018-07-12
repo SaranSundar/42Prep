ba930398ecf21500aa1afa117a372b1a2b6cbf20
Saran Sundararajan <saran@Sarans-MacBook-Pro.local>
2018-07-12 11:05:43 -0700

    Files

--- src/components/CodeIDE/CodeIDE.js
+++ src/components/CodeIDE/CodeIDE.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import "./CodeIDE.css"
-import CodeEditor from "./CodeEditor";
 
 class CodeIDE extends Component {
 
@@ -11,21 +10,22 @@ class CodeIDE extends Component {
         };
     }
 
+    componentDidMount() {
+        // setTimeout(() => {
+        //     this.loadFrame();
+        // }, 3000);
+    }
+
 
     loadFrame = () => {
         this.setState({loaded: true});
     };
 
     render() {
-        var iframe = <CodeEditor onLoad={this.loadFrame} height="650px" width="100%"
-                                 src="https://repl.it/@sarans/TreasuredEnviousFrontend?lite=true"
-                                 scrolling="no" frameBorder="no"
-                                 allowFullScreen="true"
-                                 sandbox="allow-forms allow-pointer-lock allow-popups allow-same-origin allow-scripts allow-modals"/>;
         return (
             <div>
                 {!this.state.loaded && <div id="animation">
-                    <h1 className="code-title">Calculating...Hard At Work...</h1>
+                    <h1 className="code-title">LOADING RESOURCES...</h1>
                     <svg viewBox="0 0 124 124">
                         <polygon className="hex" fill="none" stroke="#000000"
                                  points="92,48.4575131106 92,15.542486889354095 62,2 32,15.542486889354095 32,48.4575131106"/>
@@ -35,7 +35,11 @@ class CodeIDE extends Component {
                                  points="32,48.4575131106 2,62 2,94.9150262213 32,108.457513111 62,94.9150262213"/>
                     </svg>
                 </div>}
-                {this.state.loaded && iframe}
+                {<iframe onLoad={this.loadFrame} height="650px" width="100%"
+                         src="https://repl.it/@sarans/TreasuredEnviousFrontend?lite=true"
+                         scrolling="no" frameBorder="no"
+                         allowFullScreen="true"
+                         sandbox="allow-forms allow-pointer-lock allow-popups allow-same-origin allow-scripts allow-modals"/>}
             </div>
         );
     }
--- src/components/Videos/Videos.js
+++ src/components/Videos/Videos.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import "./Videos.css"
+import Carousel from "react-bootstrap/es/Carousel";
+
 
 class Videos extends Component {
 
@@ -9,6 +11,29 @@ class Videos extends Component {
         return (
             <div>
                 VIDEOS GO HERE
+                <Carousel>
+                    <Carousel.Item>
+                        <img width={900} height={500} alt="900x500" src="https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/13001000/Beagle-On-White-01-400x267.jpg" />
+                        <Carousel.Caption>
+                            <h3>First slide label</h3>
+                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img width={900} height={500} alt="900x500" src="https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/13001000/Beagle-On-White-01-400x267.jpg" />
+                        <Carousel.Caption>
+                            <h3>Second slide label</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img width={900} height={500} alt="900x500" src="https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/13001000/Beagle-On-White-01-400x267.jpg" />
+                        <Carousel.Caption>
+                            <h3>Third slide label</h3>
+                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>;
             </div>
         );
     }
