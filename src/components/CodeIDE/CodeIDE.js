3fd6d0c49d60057d35857056c455fbdc229e8e31
Saran Sundararajan <saran@Sarans-MacBook-Pro.local>
2018-07-12 10:43:47 -0700

    FRAME

--- src/components/CodeIDE/CodeIDE.js
+++ src/components/CodeIDE/CodeIDE.js
@@ -1,28 +1,41 @@
 import React, {Component} from 'react';
 import "./CodeIDE.css"
+import CodeEditor from "./CodeEditor";
 
 class CodeIDE extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            loaded: true
+            loaded: false
         };
     }
+
 
     loadFrame = () => {
         this.setState({loaded: true});
     };
 
     render() {
+        var iframe = <CodeEditor onLoad={this.loadFrame} height="650px" width="100%"
+                                 src="https://repl.it/@sarans/TreasuredEnviousFrontend?lite=true"
+                                 scrolling="no" frameBorder="no"
+                                 allowFullScreen="true"
+                                 sandbox="allow-forms allow-pointer-lock allow-popups allow-same-origin allow-scripts allow-modals"/>;
         return (
             <div>
-                {!this.state.loaded && <h1>LOADING</h1>}
-                {this.state.loaded && <iframe onLoad={this.loadFrame} height="650px" width="100%"
-                                              src="https://repl.it/@sarans/TreasuredEnviousFrontend?lite=true"
-                                              scrolling="no" frameBorder="no"
-                                              allowFullScreen="true"
-                                              sandbox="allow-forms allow-pointer-lock allow-popups allow-same-origin allow-scripts allow-modals"/>}
+                {!this.state.loaded && <div id="animation">
+                    <h1 className="code-title">Calculating...Hard At Work...</h1>
+                    <svg viewBox="0 0 124 124">
+                        <polygon className="hex" fill="none" stroke="#000000"
+                                 points="92,48.4575131106 92,15.542486889354095 62,2 32,15.542486889354095 32,48.4575131106"/>
+                        <polygon className="hex" fill="none" stroke="#000000"
+                                 points="62,94.9150262213 92,108.457513111 122,94.9150262213 122,62 92,48.4575131106"/>
+                        <polygon className="hex" fill="none" stroke="#000000"
+                                 points="32,48.4575131106 2,62 2,94.9150262213 32,108.457513111 62,94.9150262213"/>
+                    </svg>
+                </div>}
+                {this.state.loaded && iframe}
             </div>
         );
     }
