f8d47376cab8e25767f923ad408d27df9f7bbc31
Saran Sundararajan <saran@Sarans-MacBook-Pro.local>
2018-07-12 11:57:44 -0700

    Videos

--- src/components/Videos/Videos.js
+++ src/components/Videos/Videos.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import "./Videos.css"
 import Carousel from "react-bootstrap/es/Carousel";
+import YouTube from 'react-youtube';
 
 
 class Videos extends Component {
@@ -8,35 +9,59 @@ class Videos extends Component {
     state = {};
 
     render() {
+        const opts = {
+            height: '390',
+            width: '640',
+            playerVars: { // https://developers.google.com/youtube/player_parameters
+                autoplay: 1
+            }
+        };
         return (
-            <div>
-                VIDEOS GO HERE
+            <div className="Videos">
                 <Carousel>
                     <Carousel.Item>
-                        <img width={900} height={500} alt="900x500" src="https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/13001000/Beagle-On-White-01-400x267.jpg" />
-                        <Carousel.Caption>
-                            <h3>First slide label</h3>
-                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                        </Carousel.Caption>
+                        <img className="video-thumbnail" width={900} height={500} alt="900x500"
+                             src="https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/13001000/Beagle-On-White-01-400x267.jpg"/>
+                        <img className="video-thumbnail" width={900} height={500} alt="900x500"
+                             src="https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/13001000/Beagle-On-White-01-400x267.jpg"/>
+                        <img className="video-thumbnail" width={900} height={500} alt="900x500"
+                             src="https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/13001000/Beagle-On-White-01-400x267.jpg"/>
                     </Carousel.Item>
                     <Carousel.Item>
-                        <img width={900} height={500} alt="900x500" src="https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/13001000/Beagle-On-White-01-400x267.jpg" />
-                        <Carousel.Caption>
-                            <h3>Second slide label</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        </Carousel.Caption>
+                        <img className="video-thumbnail" width={900} height={500} alt="900x500"
+                             src="https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/13001000/Beagle-On-White-01-400x267.jpg"/>
+                        <img className="video-thumbnail" width={900} height={500} alt="900x500"
+                             src="https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/13001000/Beagle-On-White-01-400x267.jpg"/>
+                        <img className="video-thumbnail" width={900} height={500} alt="900x500"
+                             src="https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/13001000/Beagle-On-White-01-400x267.jpg"/>
                     </Carousel.Item>
                     <Carousel.Item>
-                        <img width={900} height={500} alt="900x500" src="https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/13001000/Beagle-On-White-01-400x267.jpg" />
-                        <Carousel.Caption>
-                            <h3>Third slide label</h3>
-                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                        </Carousel.Caption>
+                        <img className="video-thumbnail" width={900} height={500} alt="900x500"
+                             src="https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/13001000/Beagle-On-White-01-400x267.jpg"/>
+                        <img className="video-thumbnail" width={900} height={500} alt="900x500"
+                             src="https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/13001000/Beagle-On-White-01-400x267.jpg"/>
+                        <img className="video-thumbnail" width={900} height={500} alt="900x500"
+                             src="https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/13001000/Beagle-On-White-01-400x267.jpg"/>
                     </Carousel.Item>
-                </Carousel>;
+                </Carousel>
+                <div className="video-player">
+                    <YouTube
+                        videoId="2g811Eo7K8U"
+                        opts={opts}
+                        onReady={this._onReady}
+                    />
+                </div>
+                <p>HERWREW EWFJEWKF OIWEFIOEWNKFNEWPIFJ EOWJFKEWFP
+                    EWFJKEWNFEWFNO EWNFEWFPIOEWFPIWE OJWEFIE PWEKJFO PIJEWFNPJNEW
+                    JOEFNOEWFNONWFEWO</p>
             </div>
         );
     }
+
+    _onReady = (event) => {
+        // access to player in all event handlers via event.target
+        event.target.pauseVideo();
+    };
 }
 
 export default Videos;
